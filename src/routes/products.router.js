5e08c02e8c02e2744b1964f136e7f19b112e3c3e
Bautista Rampinelli <jbrampinelli@gmail.com>
2023-03-18 13:00:56 -0300

    Primer preentrega

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -5,35 +5,35 @@ import { __dirname } from "../utils";
 const router = Router();
 const productManager = new ProductManager(__dirname+'/api/products.json');
 
-app.get('/', async(req, res) => {
+router.get('/', async(req, res) => {
     const products = await productManager.getProducts();
     res.json({products})
 })
 
-app.get('/:id', async(req, res) => {
+router.get('/:id', async(req, res) => {
     const {id} = req.params;
     const product = await productManager.getProductById(+id);
     res.json({product});
 })
 
-app.post('/', async(req, res) => {
+router.post('/', async(req, res) => {
     const obj = req.body;
     const newProduct = await productManager.addProduct();
-    res.json({message: 'Product created', product: 'newProduct'});
+    res.json({message: 'Product created', product: newProduct});
 })
 
-app.delete('/', async(req, res) => {
+router.delete('/', async(req, res) => {
     const message = await productManager.deleteProducts();
     res.json({message});
 })
 
-app.delete('/:id', async(req, res) => {
+router.delete('/:id', async(req, res) => {
     const {id} = req.params;
     const message = await productManager.deleteProductById(+id);
     res.json({message});
 })
 
-app.put('/:id', async(req,res) => {
+router.put('/:id', async(req,res) => {
     const {id} = req.params;
     const obj = req.body;
     const product = await productManager.updateProduct(+id, obj);
