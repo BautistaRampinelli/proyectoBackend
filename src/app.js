dad8b1a8cff2bb5b4db3cb503fdae99267f161db
Bautista Rampinelli <jbrampinelli@gmail.com>
2023-03-18 13:03:12 -0300

    Primer preentrega proyecto final

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import ProductManager from "./ProductManager";
 import cartsRouter from './routes/carts.router';
 import productsRouter from './routes/products.router';
+import { __dirname } from "./utils";
 
 const app = express();
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.use('/public',express.static(__dirname));
 app.use('/api/products', productsRouter);
 app.use('/api/carts', cartsRouter);
 
