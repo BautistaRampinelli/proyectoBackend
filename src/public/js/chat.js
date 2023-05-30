7a7af84f31ae0941b83185ffd5153472423b4152
Bautista Rampinelli <jbrampinelli@gmail.com>
2023-05-29 21:41:53 -0300

    Desafío integrador

---
+++ src/public/js/chat.js
@@ -0,0 +1,17 @@
+// El archivo debe estar en una carpeta public
+
+const socket = io();
+
+document.getElementById('messageForm').addEventListener('submit', (e) => {
+  e.preventDefault();
+  let user = document.getElementById('user').value;
+  let message = document.getElementById('message').value;
+  socket.emit('chat message', { user, message });
+  document.getElementById('message').value = ''; 
+});
+
+socket.on('chat message', (msg) => {
+  let item = document.createElement('li');
+  item.textContent = `${msg.user}: ${msg.message}`;
+  document.getElementById('messages').appendChild(item);
+});
